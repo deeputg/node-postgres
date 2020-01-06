4af10c1962e3d5cf015a58e94411fcc57567a99e
deeputg <deeputg1992@gmail.com>
2020-01-07 01:40:26 +0530

    crete home endpoint

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json({ status: true, message: "home" })
+})
+
 PORT = process.env.PORT | 3000
 app.listen(PORT, () => {
     console.log("server listening to port ", PORT)
